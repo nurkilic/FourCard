626b6c4547f85fe0e27ed58721216711a4154961
Nur <rumeysanurkilic@gmail.com>
2024-11-10 02:24:27 +0300

    updated border-top of cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const Card = ({ header, comment, icon, color }) => {
   return (
+    <>
+    
     <div
-      style={{ borderTopColor: color }}
-      className="w-64 max-sm:mx-5 min-w-40 max-sm:w-full min-h-44  p-5 shadow-lg rounded-md  flex flex-col justify-between  border-[3px] border-solid"
+      
+      className="relative w-64 max-sm:mx-5 min-w-40 max-sm:w-full min-h-44  p-5 shadow-lg rounded-md rounded-t-none  flex flex-col justify-between "
     >
+      <div className="h-[3px] w-full rounded-t-full px-2 absolute -top-1 left-0 border border-b-transparent border-solid" style={{ backgroundColor: color }}></div>
       <div>
         <h3 className="text-[var(--very-dark-blue)] font-semibold">{header}</h3>
         <p className="text-[var(--grayish-blue)] text-xs">{comment}</p>
@@ -14,6 +17,7 @@ const Card = ({ header, comment, icon, color }) => {
         <img className="ml-auto" src={icon} alt="" />
       </div>
     </div>
+    </>
   );
 };
 
